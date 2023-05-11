7c210bd027ea9b7cb3a0e673e8f0ba1a9d27558b
Vycheslav Smirnov <smirn.006vychislavoo@gmail.com>
2023-05-11 17:11:16 +0300

    Added a division into classes in the authorization window.

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,10 @@
 import "./css/style.css";
 import "./css/normalize.css";
-import "./templates/authorisation/authorisation.css";
-import "./templates/photo/photo.css";
+import "./templates/authorisation/loginName/loginName.css";
+import "./templates/authorisation/loginPhoto/loginPhoto.css";
 import "./templates/chat/chat.css";
 import "./templates/chat/loading.css";
 
-import Authorisation from "./scripts/authorisation.js";
-// import "./templates/helpers/helper.js";
+import Authorisation from "./scripts/authorisation/authorisation.js";
 
 new Authorisation();
